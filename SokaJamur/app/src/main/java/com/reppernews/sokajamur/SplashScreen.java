d1ffa2f1b30c6b187703ae9e9b0b0094f2a9c3c5
RIZKY-PC\RIZKY <rizky27a>
2018-05-04 17:26:15 +0700

    splash screen berjalan

--- SokaJamur/app/src/main/java/com/reppernews/sokajamur/SplashScreen.java
+++ SokaJamur/app/src/main/java/com/reppernews/sokajamur/SplashScreen.java
@@ -1,8 +1,31 @@
 package com.reppernews.sokajamur;
 
-/**
- * Created by RIZKY on 5/2/2018.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashScreen {
-}
+public class SplashScreen extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent ss = new Intent(SplashScreen.this, HomeActivity.class);
+                startActivity(ss);
+                finish();
+            }
+        }, 3000L);
+
+        }
+    }
+

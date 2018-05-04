1611b2b4a2659e83d41fb56b574e854e2e40e36e
Ladeta Okta Verawan <ladetaokta05@gmail.com>
2018-05-04 15:42:53 +0700

    hapus coment yang tidak penting

--- SokaJamur/app/src/main/java/com/reppernews/sokajamur/HomeUser.java
+++ SokaJamur/app/src/main/java/com/reppernews/sokajamur/HomeUser.java
@@ -54,19 +54,14 @@ public class HomeUser extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home_user, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -77,7 +72,7 @@ public class HomeUser extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {

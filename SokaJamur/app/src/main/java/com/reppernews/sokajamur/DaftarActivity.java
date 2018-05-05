7c8ae86ac846c6f477ab0cff7cbe81248595d99f
Arif Kh <arifkhoiruman2@gmail.com>
2018-05-05 18:22:02 +0700

    register done

--- SokaJamur/app/src/main/java/com/reppernews/sokajamur/DaftarActivity.java
+++ SokaJamur/app/src/main/java/com/reppernews/sokajamur/DaftarActivity.java
@@ -1,33 +1,179 @@
 package com.reppernews.sokajamur;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.reppernews.sokajamur.app.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DaftarActivity extends AppCompatActivity {
+    ProgressDialog pDialog;
+    EditText isianNama, isianAlm, isianNo, isianEmail, isianPass;
+    int success;
+    ConnectivityManager conMgr;
+    Intent intent;
+
+    private String url = Server.URL + "register.php";
+
+    private static final String TAG = DaftarActivity.class.getSimpleName();
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    String tag_json_obj = "json_obj_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
+        conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        {
+            if (conMgr.getActiveNetworkInfo() != null
+                    && conMgr.getActiveNetworkInfo().isAvailable()
+                    && conMgr.getActiveNetworkInfo().isConnected()) {
+            } else {
+                Toast.makeText(getApplicationContext(), "No Internet Connection",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
 
-         EditText isianNama= (EditText) findViewById(R.id.isianNama);
-         EditText isianAlm = (EditText) findViewById(R.id.isianAlm);
-         EditText isianNo = (EditText) findViewById(R.id.isianNo);
-         EditText isianEmail = (EditText) findViewById(R.id.isianEmail);
-         EditText isianPass = (EditText) findViewById(R.id.isianPass);
+           isianNama= (EditText) findViewById(R.id.isianNama);
+           isianAlm = (EditText) findViewById(R.id.isianAlm);
+           isianNo = (EditText) findViewById(R.id.isianNo);
+           isianEmail = (EditText) findViewById(R.id.isianEmail);
+           isianPass = (EditText) findViewById(R.id.isianPass);
         Button btndaftar = (Button) findViewById(R.id.btndaftar);
+
 
         btndaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String nama = isianNama.getText().toString();
+                String alamat = isianAlm.getText().toString();
+                String nohp = isianNo.getText().toString();
+                String email  = isianEmail.getText().toString();
+                String password = isianPass.getText().toString();
+                if (conMgr.getActiveNetworkInfo() != null
+                        && conMgr.getActiveNetworkInfo().isAvailable()
+                        && conMgr.getActiveNetworkInfo().isConnected()) {
+                    checkRegister(nama, alamat, nohp, email, password);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_SHORT).show();
+                }
 
-                Intent intent = new Intent(getApplicationContext(),HomeUser.class);
-                startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(),HomeUser.class);
+                //startActivity(intent);
             }
         });
+
     }
+    private void checkRegister(final String nama,final  String alamat,final  String nohp,final  String email,final  String password){
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Register ...");
+        showDialog();
+        StringRequest strReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "Register Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    success = jObj.getInt(TAG_SUCCESS);
+
+                    // Check for error node in json
+                    if (success == 1) {
+
+                        Log.e("Successfully Register!", jObj.toString());
+
+                        Toast.makeText(getApplicationContext(),
+                                jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                        isianNama.setText("");
+                        isianAlm.setText("");
+                        isianNo.setText("");
+                        isianEmail.setText("");
+                        isianPass.setText("");
+                        //txt_confirm_password.setText("");
+
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+
+                hideDialog();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("nama", nama);
+                params.put("password", password);
+                params.put("email", email);
+                params.put("alamat", alamat);
+                params.put("no", nohp);
+                //params.put("confirm_password", confirm_password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    @Override
+    public void onBackPressed() {
+        intent = new Intent(DaftarActivity.this, LoginActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
 }

b06b79fb6759f674b18d8eba13abc0c12f2d1ced
Arif Kh <arifkhoiruman2@gmail.com>
2018-05-07 23:42:58 +0700

    perbaikan bug, tapi masih ngebug

--- SokaJamur/app/src/main/java/com/reppernews/sokajamur/HomeAdmin.java
+++ SokaJamur/app/src/main/java/com/reppernews/sokajamur/HomeAdmin.java
@@ -1,5 +1,6 @@
 package com.reppernews.sokajamur;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,18 +81,14 @@ public class HomeAdmin extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_infoBarangadmin) {
+            Intent intent = new Intent(getApplicationContext(),AdminBarang.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_pesananPembeli) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logoutAdmin) {
+            Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);

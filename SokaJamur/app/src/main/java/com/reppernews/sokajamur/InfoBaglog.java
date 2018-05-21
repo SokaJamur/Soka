3dbe279d16fb0c5ae209181301b6b3f2a43f18dc
Ladeta Okta Verawan <ladetaokta05@gmail.com>
2018-05-21 21:44:07 +0700

    layout info jamur & baglog fix tah?

--- SokaJamur/app/src/main/java/com/reppernews/sokajamur/InfoBaglog.java
+++ SokaJamur/app/src/main/java/com/reppernews/sokajamur/InfoBaglog.java
@@ -1,7 +1,9 @@
 package com.reppernews.sokajamur;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,9 +11,12 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import static com.reppernews.sokajamur.LoginActivity.my_shared_preferences;
+
 public class InfoBaglog extends AppCompatActivity {
     private Spinner combobox2;
     private Button btpilih2;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,7 @@ public class InfoBaglog extends AppCompatActivity {
         String[] pilihan_menu2=getResources().getStringArray(R.array.pilihan_menu2); // ambil menu dari string.xml
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.item_spin,R.id.txItemSpin, pilihan_menu2);
         combobox2.setAdapter(adapter);
+        sharedPreferences = getSharedPreferences(my_shared_preferences, Context.MODE_PRIVATE);
 
         combobox2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
@@ -40,6 +46,10 @@ public class InfoBaglog extends AppCompatActivity {
         });
 
     }
+
     public void klikPilih(View v){
         String pilihan=combobox2.getSelectedItem().toString();
-        Toast.makeText(this,pilihan,Toast.LENGTH_SHORT).show();}}
+        Toast.makeText(this,pilihan,Toast.LENGTH_SHORT).show();
+    }
+}
+

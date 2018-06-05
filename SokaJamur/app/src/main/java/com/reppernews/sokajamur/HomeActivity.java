7c5b0822128dee1e152d375245aaea0af48ac411
DESKTOP-9I5SQ1A\Fatur <faturhermawan18januari1998@gmail.com>
2018-06-05 16:17:46 +0700

    embo opo iki

--- SokaJamur/app/src/main/java/com/reppernews/sokajamur/HomeActivity.java
+++ SokaJamur/app/src/main/java/com/reppernews/sokajamur/HomeActivity.java
@@ -198,7 +198,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(HomeActivity.this, InfoBaglog.class);
+        Intent intent = new Intent(HomeActivity.this, Artikel.class);
 //        HashMap<String,String> map=(HashMap)parent.getItemAtPosition(position);
 //        String id_artikel = map.get(TAG_ID_ARTIKEL).toString();
 //        intent.putExtra(TAG_ID_ARTIKEL,id_artikel);
